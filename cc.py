35454fe8b9981b554cbb52879867e8fe72ee6bfa
Pranav Yadav <128580103+pranav-ydv11@users.noreply.github.com>
2024-04-23 12:14:43 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -1,1 +1,10 @@
-print("hello world")
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Hello World")
+
+app = webapp2.WSGIApplication([
+    ("/", MainPage),
+], debug=True)
+
